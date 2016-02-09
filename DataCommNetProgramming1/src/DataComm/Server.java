3a2a10bf6325985df5b700f9304f8f7e1f8e66e1
Lucas Andrade <passaroalado@hotmail.com>
2016-02-08 23:50:00 -0600

    set up project

---
+++ DataCommNetProgramming1/src/DataComm/Server.java
@@ -0,0 +1,14 @@
+package DataComm;
+
+public class Server {
+
+	public Server() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
